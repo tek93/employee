30b9413186e44ee8cf7f61e18ae1de97b09814c8
tek93 <=>
2021-03-14 21:24:08 +0100

    Add new files

---
+++ src/main/java/tech/getarrays/employeemanager/model/Employee.java
@@ -0,0 +1,32 @@
+package tech.getarrays.employeemanager.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Employee implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(nullable = false, updatable = false)
+    private Long id;
+    private String name;
+    private String email;
+    private String jobTitle;
+    private String phone;
+    private String imageUrl;
+    @Column(nullable = false, updatable = false)
+    private String employeeCode;
+    @Column(name = "picByte", length = 10000)
+    private byte[] picByte;
+
+
+}
---
+++ src/main/java/tech/getarrays/employeemanager/resource/EmployeeResource.java
@@ -0,0 +1,62 @@
+package tech.getarrays.employeemanager.resource;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import tech.getarrays.employeemanager.model.Employee;
+import tech.getarrays.employeemanager.service.EmployeeService;
+
+import java.io.IOException;
+import java.util.List;
+
+@RestController
+@RequestMapping("/employee")
+public class EmployeeResource {
+    private final EmployeeService employeeService;
+    private byte[] bytes;
+
+    public EmployeeResource(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @PostMapping("/upload")
+    public void uploadImage(@RequestParam("imageFile") MultipartFile file) throws IOException {
+        this.bytes = file.getBytes();
+    }
+
+
+    @GetMapping("/all")
+    public ResponseEntity<List<Employee>> getAllEmployees () {
+        List<Employee> employees = employeeService.findAllEmployees();
+        return new ResponseEntity<>(employees, HttpStatus.OK);
+    }
+
+    @GetMapping("/find/{id}")
+    public ResponseEntity<Employee> getEmployeeById (@PathVariable("id") Long id) {
+        Employee employee = employeeService.findEmployeeById(id);
+        return new ResponseEntity<>(employee, HttpStatus.OK);
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<Employee> addEmployee(@RequestBody Employee employee) {
+        Employee newEmployee = employeeService.addEmployee(employee);
+        employee.setPicByte(this.bytes);
+
+        this.bytes = null;
+
+        return new ResponseEntity<>(newEmployee, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<Employee> updateEmployee(@RequestBody Employee employee) {
+        Employee updateEmployee = employeeService.updateEmployee(employee);
+        return new ResponseEntity<>(updateEmployee, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<?> deleteEmployee(@PathVariable("id") Long id) {
+        employeeService.deleteEmployee(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+}
