135541b43ba716aa718a3f43cfcac59ba580ac98
tek93 <teka93@o2.pl>
2021-07-28 22:01:48 +0200

    Update EmployeeResource.java

--- src/main/java/tech/getarrays/employeemanager/resource/EmployeeResource.java
+++ src/main/java/tech/getarrays/employeemanager/resource/EmployeeResource.java
@@ -12,7 +12,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/employee")
-@CrossOrigin
+@CrossOrigin(origins = "*")
 public class EmployeeResource {
     private final EmployeeService employeeService;
     private byte[] bytes;
@@ -20,41 +20,42 @@ public class EmployeeResource {
     public EmployeeResource(EmployeeService employeeService) {
         this.employeeService = employeeService;
     }
-    @CrossOrigin
+   
     @PostMapping("/upload")
     public void uploadImage(@RequestParam("imageFile") MultipartFile file) throws IOException {
         this.bytes = file.getBytes();
     }
 
-    @CrossOrigin
+  
     @GetMapping("/all")
     public ResponseEntity<List<Employee>> getAllEmployees () {
         List<Employee> employees = employeeService.findAllEmployees();
         return new ResponseEntity<>(employees, HttpStatus.OK);
     }
-@CrossOrigin
+
     @GetMapping("/find/{id}")
     public ResponseEntity<Employee> getEmployeeById (@PathVariable("id") Long id) {
         Employee employee = employeeService.findEmployeeById(id);
         return new ResponseEntity<>(employee, HttpStatus.OK);
     }
-@CrossOrigin
+
     @PostMapping("/add")
     public ResponseEntity<Employee> addEmployee(@RequestBody Employee employee) {
+          employee.setPicByte(this.bytes);
         Employee newEmployee = employeeService.addEmployee(employee);
-        employee.setPicByte(this.bytes);
+      
 
         this.bytes = null;
 
         return new ResponseEntity<>(newEmployee, HttpStatus.CREATED);
     }
-@CrossOrigin
+
     @PutMapping("/update")
     public ResponseEntity<Employee> updateEmployee(@RequestBody Employee employee) {
         Employee updateEmployee = employeeService.updateEmployee(employee);
         return new ResponseEntity<>(updateEmployee, HttpStatus.OK);
     }
-@CrossOrigin
+
     @DeleteMapping("/delete/{id}")
     public ResponseEntity<?> deleteEmployee(@PathVariable("id") Long id) {
         employeeService.deleteEmployee(id);
